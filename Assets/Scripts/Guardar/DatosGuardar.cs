c875fdb45d204fd03a6167b6eb669bde2521e9b0
SebastianEsatupinan <124362855+SebastianEsatupinan@users.noreply.github.com>
2023-11-15 19:44:41 -0500

    Agregado el nivel 1 de miguel

---
+++ Assets/Scripts/Guardar/DatosGuardar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[SerializeField]
+public class DatosGuardar 
+{
+    public int tiempo;
+    public string nombreU;
+
+
+}
